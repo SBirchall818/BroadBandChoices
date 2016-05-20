ed25b6928e3d684e0dfdbc0a9b54d8aee8f2a717
Simon Birchall <simon.birchall.22@gmail.com>
2016-05-20 08:38:04 +0100

    Add Unit test project for BasketUI

---
+++ BasketUI/BasketUITests/SetupTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasketUI;
+
+namespace BasketUITests
+{
+    [TestClass]
+    public class SetupTests
+    {
+        [TestMethod]
+        public void BasketUICanBeInitializedWithABasket()
+        {
+            BasketForm basketForm = new BasketForm(new Basket());
+        }
+    }
+}
