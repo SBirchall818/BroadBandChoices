c18a18510af280c25342b4f8820c59c9ac2f5ebf
Simon Birchall <simon.birchall.22@gmail.com>
2016-05-20 15:42:39 +0100

    Testing bread and butter rule logic

--- BasketUI/BasketLogicTests/TwoButterGetABread50PerCentOffTests.cs
+++ BasketUI/BasketLogicTests/TwoButterGetABread50PerCentOffTests.cs
@@ -35,5 +35,195 @@ namespace BasketLogicTests
             float expected = -(breadItem.Cost / 2.0f);
             Assert.AreEqual(expected, result);
         }
+
+
+        [TestMethod]
+        public void AppliesIfFourButterAndSingleBreadInBasket()
+        {
+            TwoButterGetABread50PerCentOff rule = new TwoButterGetABread50PerCentOff();
+            List<BasketItem> basketItems = new List<BasketItem>();
+            ButterItem butterItemA = new ButterItem();
+            ButterItem butterItemB = new ButterItem();
+            ButterItem butterItemC = new ButterItem();
+            ButterItem butterItemD = new ButterItem();
+            BreadItem breadItem = new BreadItem();
+            basketItems.Add(butterItemA);
+            basketItems.Add(butterItemB);
+            basketItems.Add(butterItemC);
+            basketItems.Add(butterItemD);
+            basketItems.Add(breadItem);
+
+            float result = rule.Apply(basketItems);
+            float expected = -(breadItem.Cost / 2.0f);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AppliesIfFourButterAndTwoBreadInBasket()
+        {
+            TwoButterGetABread50PerCentOff rule = new TwoButterGetABread50PerCentOff();
+            List<BasketItem> basketItems = new List<BasketItem>();
+            ButterItem butterItemA = new ButterItem();
+            ButterItem butterItemB = new ButterItem();
+            ButterItem butterItemC = new ButterItem();
+            ButterItem butterItemD = new ButterItem();
+            BreadItem breadItemA = new BreadItem();
+            BreadItem breadItemB = new BreadItem();
+            BreadItem breadItemC = new BreadItem();
+            basketItems.Add(butterItemA);
+            basketItems.Add(butterItemB);
+            basketItems.Add(butterItemC);
+            basketItems.Add(butterItemD);
+            basketItems.Add(breadItemA);
+            basketItems.Add(breadItemB);
+            basketItems.Add(breadItemC);
+
+            float result = rule.Apply(basketItems);
+            float expected = -((breadItemA.Cost + breadItemB.Cost) / 2.0f);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AppliesIfFourButterAndThreeBreadInBasket()
+        {
+            TwoButterGetABread50PerCentOff rule = new TwoButterGetABread50PerCentOff();
+            List<BasketItem> basketItems = new List<BasketItem>();
+            ButterItem butterItemA = new ButterItem();
+            ButterItem butterItemB = new ButterItem();
+            ButterItem butterItemC = new ButterItem();
+            ButterItem butterItemD = new ButterItem();
+            BreadItem breadItemA = new BreadItem();
+            BreadItem breadItemB = new BreadItem();
+            basketItems.Add(butterItemA);
+            basketItems.Add(butterItemB);
+            basketItems.Add(butterItemC);
+            basketItems.Add(butterItemD);
+            basketItems.Add(breadItemA);
+            basketItems.Add(breadItemB);
+
+            float result = rule.Apply(basketItems);
+            float expected = -((breadItemA.Cost + breadItemB.Cost) / 2.0f);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AppliesIfTwoButterAndBreadInBasketMilkHasNoEffect()
+        {
+            TwoButterGetABread50PerCentOff rule = new TwoButterGetABread50PerCentOff();
+            List<BasketItem> basketItems = new List<BasketItem>();
+            ButterItem butterItemA = new ButterItem();
+            ButterItem butterItemB = new ButterItem();
+            BreadItem breadItem = new BreadItem();
+            MilkItem milkItemA = new MilkItem();
+            MilkItem milkItemB = new MilkItem();
+            MilkItem milkItemC = new MilkItem();
+            MilkItem milkItemD = new MilkItem();
+            basketItems.Add(milkItemA);
+            basketItems.Add(milkItemB);
+            basketItems.Add(milkItemC);
+            basketItems.Add(milkItemD);
+
+            basketItems.Add(butterItemA);
+            basketItems.Add(butterItemB);
+            basketItems.Add(breadItem);
+
+            float result = rule.Apply(basketItems);
+            float expected = -(breadItem.Cost / 2.0f);
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+        public void AppliesIfFourButterAndSingleBreadInBasketMilkHasNoEffect()
+        {
+            TwoButterGetABread50PerCentOff rule = new TwoButterGetABread50PerCentOff();
+            List<BasketItem> basketItems = new List<BasketItem>();
+            ButterItem butterItemA = new ButterItem();
+            ButterItem butterItemB = new ButterItem();
+            ButterItem butterItemC = new ButterItem();
+            ButterItem butterItemD = new ButterItem();
+            BreadItem breadItem = new BreadItem();
+            MilkItem milkItemA = new MilkItem();
+            MilkItem milkItemB = new MilkItem();
+            MilkItem milkItemC = new MilkItem();
+            MilkItem milkItemD = new MilkItem();
+            basketItems.Add(milkItemA);
+            basketItems.Add(milkItemB);
+            basketItems.Add(milkItemC);
+            basketItems.Add(milkItemD);
+            basketItems.Add(butterItemA);
+            basketItems.Add(butterItemB);
+            basketItems.Add(butterItemC);
+            basketItems.Add(butterItemD);
+            basketItems.Add(breadItem);
+
+            float result = rule.Apply(basketItems);
+            float expected = -(breadItem.Cost / 2.0f);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AppliesIfFourButterAndTwoBreadInBasketMilkHasNoEffect()
+        {
+            TwoButterGetABread50PerCentOff rule = new TwoButterGetABread50PerCentOff();
+            List<BasketItem> basketItems = new List<BasketItem>();
+            ButterItem butterItemA = new ButterItem();
+            ButterItem butterItemB = new ButterItem();
+            ButterItem butterItemC = new ButterItem();
+            ButterItem butterItemD = new ButterItem();
+            BreadItem breadItemA = new BreadItem();
+            BreadItem breadItemB = new BreadItem();
+            BreadItem breadItemC = new BreadItem();
+            MilkItem milkItemA = new MilkItem();
+            MilkItem milkItemB = new MilkItem();
+            MilkItem milkItemC = new MilkItem();
+            MilkItem milkItemD = new MilkItem();
+            basketItems.Add(milkItemA);
+            basketItems.Add(milkItemB);
+            basketItems.Add(milkItemC);
+            basketItems.Add(milkItemD);
+            basketItems.Add(butterItemA);
+            basketItems.Add(butterItemB);
+            basketItems.Add(butterItemC);
+            basketItems.Add(butterItemD);
+            basketItems.Add(breadItemA);
+            basketItems.Add(breadItemB);
+            basketItems.Add(breadItemC);
+
+            float result = rule.Apply(basketItems);
+            float expected = -((breadItemA.Cost + breadItemB.Cost) / 2.0f);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AppliesIfFourButterAndThreeBreadInBasketMilkHasNoEffect()
+        {
+            TwoButterGetABread50PerCentOff rule = new TwoButterGetABread50PerCentOff();
+            List<BasketItem> basketItems = new List<BasketItem>();
+            ButterItem butterItemA = new ButterItem();
+            ButterItem butterItemB = new ButterItem();
+            ButterItem butterItemC = new ButterItem();
+            ButterItem butterItemD = new ButterItem();
+            BreadItem breadItemA = new BreadItem();
+            BreadItem breadItemB = new BreadItem();
+            MilkItem milkItemA = new MilkItem();
+            MilkItem milkItemB = new MilkItem();
+            MilkItem milkItemC = new MilkItem();
+            MilkItem milkItemD = new MilkItem();
+            basketItems.Add(milkItemA);
+            basketItems.Add(milkItemB);
+            basketItems.Add(milkItemC);
+            basketItems.Add(milkItemD);
+            basketItems.Add(butterItemA);
+            basketItems.Add(butterItemB);
+            basketItems.Add(butterItemC);
+            basketItems.Add(butterItemD);
+            basketItems.Add(breadItemA);
+            basketItems.Add(breadItemB);
+
+            float result = rule.Apply(basketItems);
+            float expected = -((breadItemA.Cost + breadItemB.Cost) / 2.0f);
+            Assert.AreEqual(expected, result);
+        }
     }
 }
