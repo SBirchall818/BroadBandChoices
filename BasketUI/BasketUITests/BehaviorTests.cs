f0e821c3720f08e67de4476670d91587aa5c3e1b
Simon Birchall <simon.birchall.22@gmail.com>
2016-05-20 10:20:34 +0100

    Addition of BehaviorTests with failing test for behavior of a click on ButterButton

---
+++ BasketUI/BasketUITests/BehaviorTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasketUI;
+using Moq;
+using BasketLogic.Basket;
+
+namespace BasketUITests
+{
+    [TestClass]
+    public class BehaviorTests
+    {
+        [TestMethod]
+        public void ClickingButterButtonShouldAddItemToBasket()
+        {
+            Mock<IBasket> mockBasket = new Mock<IBasket>();
+            BasketForm basketForm = new BasketForm(mockBasket.Object);
+            basketForm.ButterButton_Click(null, null);
+        }
+    }
+}
