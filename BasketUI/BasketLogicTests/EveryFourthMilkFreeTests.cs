1bc3c9128cb9eab3b67fa8cdd2ce994cb3998e16
Simon Birchall <simon.birchall.22@gmail.com>
2016-05-20 15:46:05 +0100

    Create failing test for every fourth milk free rule

---
+++ BasketUI/BasketLogicTests/EveryFourthMilkFreeTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasketLogic.BasketItems;
+using System.Collections.Generic;
+
+namespace BasketLogicTests
+{
+    [TestClass]
+    public class EveryFourthMilkFreeTests
+    {
+        [TestMethod]
+        public void DoesNotApplyIfListIsEmpty()
+        {
+            EveryFourthMilkFree rule = new EveryFourthMilkFree();
+            List<BasketItem> basketItems = new List<BasketItem>();
+
+            float result = rule.Apply(basketItems);
+            Assert.AreEqual(0.0f, result);
+        }
+    }
+}
