90940ed7c58d33baadd6ed0a01270b1483b11d2f
Simon Birchall <simon.birchall.22@gmail.com>
2016-05-20 13:16:03 +0100

    Add failing test for rules engine ApplyRules method

---
+++ BasketUI/BasketLogicTests/RulesEngineTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BasketLogicTests
+{
+    [TestClass]
+    public class RulesEngineTest
+    {
+        [TestMethod]
+        public void RulesEngineHasApplyRulesMethod()
+        {
+            Mock<IRulesEngine> mockRulesEngine = new Mock<IRulesEngine>();
+        }
+    }
+}
