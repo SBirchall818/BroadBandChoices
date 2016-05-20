31be358b03b60ead4ac8d9841d8026a5c1ac20e1
Simon Birchall <simon.birchall.22@gmail.com>
2016-05-20 11:41:25 +0100

    Create failing test for adding item to basket logic

---
+++ BasketUI/BasketLogicTests/BasketTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasketLogicTests
+{
+    [TestClass]
+    public class BasketTests
+    {
+        [TestMethod]
+        public void ShouldBeAbleToAddBreadItemToBasket()
+        {
+            Basket basket = new Basket();
+            Assert.IsNotNull(basket.Items)
+        }
+    }
+}
