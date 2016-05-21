8c3fb585ee7a03f87f7e91f9b061af6689af3cd7
Simon Birchall <simon.birchall.22@gmail.com>
2016-05-21 11:49:48 +0100

    Add failing integration test for basket and rules

---
+++ BasketUI/BasketLogicTests/BasketRulesIntegrationTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasketLogic.Basket;
+using BasketLogic.BasketItems;
+
+namespace BasketLogicTests
+{
+    [TestClass]
+    public class BasketRulesIntegrationTests
+    {
+        [TestMethod]
+        public void BasketHas1Bread1Butter1Milk()
+        {
+            Basket basket = new Basket();
+            basket.AddItem(new BreadItem());
+            basket.AddItem(new ButterItem());
+            basket.AddItem(new MilkItem());
+
+            decimal total = basket.Total;
+            Assert.AreEqual(2.95m, total);
+        }
+    }
+}
